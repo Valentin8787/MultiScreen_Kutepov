134ebff49cd0d215b16bf858646c75e174494b30
Valentin8787 <Valentin8708@mail.ru>
2020-11-12 13:02:47 +0300

    Initial commit

---
+++ app/src/main/java/com/example/multiscreen_Kutepov/Urugvai.java
@@ -0,0 +1,16 @@
+package com.example.multiscreen_Kutepov;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.multiscreen.R;
+
+public class Urugvai extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_urugvai);
+    }
+}
